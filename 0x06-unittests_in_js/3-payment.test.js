e294664abb924c088b448c47dabf1bfcb2f18953
huzaifa250 <huz.dark1@gmail.com>
2024-11-28 20:26:20 +0000

    edit test fail 3-5

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,11 +5,11 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigspy = sinon.spy(Utils);
+    const bigSpy = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigspy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigspy.calculateNumber.callCount).to.be.equal(1);
-    bigspy.calculateNumber.restore();
+    expect(bigSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigSpy.calculateNumber.callCount).to.be.equal(1);
+    bigSpy.calculateNumber.restore();
   });
 });
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,22 +1,24 @@
-const Utils = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
+const Utils = {
+  calculateNumber(type, a, b) {
+   if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+   }
 
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
+   if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+   }
 
-  if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
-      return "Error";
+   if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return "Error";
     } else {
-      return Math.round(a) / Math.round(b);
+        return Math.round(a) / Math.round(b);
+      } 
     }
-  }
 
-  throw new Error("Invalid operation type"); /*unsuported values */
+    throw new Error("Invalid operation type"); /*unsuported values */
+  }
 };
 
-module.exports = Utils;
+  module.exports = Utils;
 
