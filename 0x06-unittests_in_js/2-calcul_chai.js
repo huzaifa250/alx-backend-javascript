2d19bed37363f9ab7fea2eb2af2c3f5a8aac656a
huzaifa250 <huz.dark1@gmail.com>
2024-11-28 19:40:27 +0000

    task 2-4 without hook

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,21 @@
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return "Error";
+    } else {
+      return Math.round(a) / Math.round(b);
+    }
+  }
+
+  throw new Error("Invalid operation type");
+};
+
+module.exports = calculateNumber;
